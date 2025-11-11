7159e8748a580dfb05b1987386ffd057e8902ab0
parth-vasoya-prominentpixel <parth.vasoya@prominentpixel.com>
2025-11-11 12:57:56 +0530

    Added waf for testing

--- resources/waf.tf
+++ resources/waf.tf
@@ -1,15 +1,13 @@
 module "console_portal_waf" {
   source  = "aws-ss/wafv2/aws"
   version = "3.13.0"
-  
+
   name        = format("%s-waf-%s-cfnt-waf-%s-%s-%s", var.prefix_company, var.application, local.region_prefixes, var.env, var.random_suffix)
   description = "WAFv2 to allow only US and India traffic for Console Portal"
 
-  scope = "CLOUDFRONT"
-resource_arn = []
-  default_action = {
-    block = {}
-  }
+  scope          = "CLOUDFRONT"
+  resource_arn   = []
+  default_action = "block"
 
   visibility_config = {
     cloudwatch_metrics_enabled = true
